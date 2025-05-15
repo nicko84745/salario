4f6173f8177adbf4f89a355d77a96bcdc64db372
NICKOLAS ADRIANO <nickolasadrianodeandradedias@gmail.com>
2025-05-15 15:15:39 -0300

    salario_de_vendedor

---
+++ Salario.py
@@ -0,0 +1,16 @@
+# Leitura dos dados
+nome = input("Digite o nome do vendedor: ")
+salario_fixo = float(input("Digite o salário fixo:"))
+total_vendas = float(input("Digite o total de vendas do mês: "))
+
+# Cálculo da comissão (15% sobre as vendas)
+
+# Cálculo do total a receber
+
+# Saída formatada
+
+print(nome,)
+
+total_receber = salario_fixo + ((total_vendas * 15)/100)
+print(nome)
+print(f"total a receber é : = R$ {total_receber:.2f}")
